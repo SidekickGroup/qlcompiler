d4dfc425110e11a6c51715ff1c1582349eaa8bd0
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-11-25 10:34:42 -0200

    Reestructuring the project

--- src/qlcompiler/js_compiler.py
+++ src/qlcompiler/js_compiler.py
@@ -1,81 +1,106 @@
 from compiler import Compiler
 import ox
+import re
+import inspect #inspect.getsource
+
+from sidekick.placeholder import *
 
 class JsCompiler(Compiler):
 
     """
     Javascript compiler.
     """
-    # Lexer
-    tokens_list = [
-        'NUMBER',
-        'STRINGS',
-        'BOOLEAN',
-        'ATRIB_OP',
-        'SIMPLE_OP',
-        'MUL_OP',
-        'BREAKLINE',
-        'INDENT',
-        'COND_IF',
-        'COND_ELIF',
-        'COND_ELSE',
-        'LOOP_FOR',
-        'LOOP_WHILE',
-        'COLON',
-        'BO_EQUAL',
-        'BO_BIGGER',
-        'BO_BIGGER_EQUAL',
-        'BO_LOWER',
-        'BO_LOWER_EQUAL',
-        'LO_NOT',
-        'LO_AND',
-        'LO_OR',
-        'VARIABLE',
-        'RW_IN',
-        'RW_IS',
-        'RW_RANGE'
-    ]
-
-
-    # List and Dictionary : To Map Sidekick - Backlog   
-    def js_lexer():
-        lexer = ox.make_lexer([
-            ('NUMBER', r'[+-]?\d+(?:\.\d+)'),
-            ('STRING', r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
-            ('BOOLEAN', r'(\ATrue)|(\AFalse)'),
-            ('ATRIB_OP', r'[\={1}]'),
-            ('SIMPLE_OP', r'[-+]'),
-            ('MUL_OP', r'[*/]'),
-            ('BREAKLINE', r'[\n\r]'), #
-            ('INDENT', r'[\t]'), #
-            ('COND_IF', r'^\s*if') #,
-            ('COND_ELIF', r'^\s*elif'), #
-            ('COND_ELSE', r'^\s*else'), #
-            ('LOOP_FOR', r'^\s*for'), 
-            ('LOOP_WHILE', r'^\s*while'),
-            ('COLON', r':$'),
-            ('BO_EQUAL', r'=='),
-            ('BO_BIGGER', r'>'),
-            ('BO_BIGGER_EQUAL', r'>='),
-            ('BO_LOWER', r'<'),
-            ('BO_LOWER_EQUAL', r'<='),
-            ('LO_NOT', r'(not)'), 
-            ('LO_AND', r'(and)'),
-            ('LO_OR', r'(or)'),
-            ('VARIABLE', r'([a-zA-Z]|_)\w+'),
-            ('RW_IN', r'(in)'),
-            ('RW_IS', r'(is)'),
-            ('RW_RANGE', r'(range)')
-        ])
-
-        return lexer
-      
-
 
 def compile(ql, **kwargs):
     """
     Compiles quick lambda object to Javascript.
     """
 
+    _input = str('ql')
+
+    parse(_input)
     compiler = JsCompiler(ql)
     return compiler.compile(**kwargs)
+
+class Ast(opt.BinOp(callable, object, object)
+          | opt.SingleOp(callable, object)
+          | opt.Call(object, tuple, dict)
+          | opt.GetAttr(object, str)
+          | opt.Placeholder(int)
+          | opt.Cte(object)):
+    """
+    AST node for a placeholder expression.
+    """
+
+
+
+tokens = [
+    'OP',
+    'BREAKLINE',
+    'ATRIB_OP',
+    'BOOLEAN',
+    'STRINGS',
+    'NUMBER',
+]
+
+# Quick lambdas
+
+# >>> inc = fn(_ + 1)
+# >>> total_cost = fn(_.num_items * _.price)
+
+'''
+    A Parser to emit js code from python
+'''
+
+# Parser Operations
+
+def js_parser(tokens):
+    parser = ox.make_parser([
+        ('term : term OP atom', binary_operator),
+        ('term : atom', lambda x: x),
+        ('atom : BREAKLINE', lambda x: x),
+        ('atom : BOOLEAN', parse_booleans),
+        ('atom : ATRIB_OP', lambda x: x),
+        ('atom : STRINGS', lambda x: str(x)),
+        ('atom : NUMBER', lambda x: float(x))
+    ], tokens)
+
+    return parser
+
+
+
+def parse_booleans(binary):
+    if binary:
+        binary = 'true'
+    else:
+        binary = 'false'
+
+    return binary
+
+
+def parse_operations(first_value, second_value, operator):
+
+    if operator is '+' or operator is '-' or operator is '*' or operator is '/':
+        return binary_operator(first_value, operator, second_value)
+
+# Binary Operator
+
+OPERATORS = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+
+def binary_operator (first_value, operator, second_value):
+    return Ast.BinOp(OPERATORS[operator],  first_value, second_value)
+
+# Backlog
+
+def parse_expressions(statements):
+    return "Some expression in JS"
+
+# Evaluate
+
+def evaluate(tree):
+    return 'a'
